0a217911dd149a0c93b65f4f43d39c2d607571cc
Larry DeGraw <larrydegraw@gmail.com>
2022-08-17 15:03:52 -0600

    Fixed stuff and got debugger running.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # vivint-assessment-fail
 A coding test I was given for a job interview that I blew.
+
+This was task to see if I knew anything. Clearly, I don't. It
+took way too long. I fumbled around.
+Requirements: Given an array of values, drop the two lowest
+numbers and average the remaining.
+
+Apparently I missed a requrement that the values in this array were supposed to be strings.
+let scores = [4, 1, 6, 10, 2, 3]
+
+Remove the two lowest values. I could sort them and remove the two lowest values, then count them and divide that total by the array length.
+sort() sorted these alphabetically (1, 10, 2, 3...). I can't believe there isn't something that does it correct. This solution came from stack overflow.
+scores.sort(function(a,b) {
+   return a - b
+})
+
+Originally I did splice(2), which in the console returned the remaining numbers I wanted. But the values contained in 'scores' were the first two numbers. I realized I needed to give the starting position and the lenght the splice out. Need to find out why the console returned the wrong contents.
+scores.splice(0,2)
+
+let total = 0
+
+I fumbled with this for a while. The code worked but didn't produce the expected result due to the splicing error before. I assumed I was doing the forEach wrong. There's probably a better way to get the totals from the array, like reduce().
+scores.forEach(element => {
+   total += element
+});
+
+let average = total / scores.length
--- app.js
+++ app.js
@@ -1,34 +1,4 @@
-// This was task to see if I knew anything. Clearly, I don't. It
-// took way too long. I fumbled around.
-// Requirements: Given an array of values, drop the two lowest
-// numbers and average the remaining.
-
-// Apparently I missed a requrement that the values in this array were supposed to be strings.
-//let scores = [4, 1, 6, 10, 2, 3]
-
-// Remove the two lowest values. I could sort them and remove the two lowest values, then count them and divide that total by the array length.
-// sort() sorted these alphabetically (1, 10, 2, 3...). I can't believe there isn't something that does it correct. This solution came from stack overflow.
-//scores.sort(function(a,b) {
-//    return a - b
-//})
-
-// Originally I did splice(2), which in the console returned the remaining numbers I wanted. But the values contained in 'scores' were the first two numbers. I realized I needed to give the starting position and the lenght the splice out. Need to find out why the console returned the wrong contents.
-//scores.splice(0,2)
-
-//let total = 0
-
-// I fumbled with this for a while. The code worked but didn't produce the expected result due to the splicing error before. I assumed I was doing the forEach wrong. There's probably a better way to get the totals from the array, like reduce().
-//scores.forEach(element => {
-//    total += element
-//});
-
-//let average = total / scores.length
-
-//////////////////////////////////////////////////////
-
 // REFACTORED VERSION
-// Possible flaws?
-// What if the values are non-numeric?
 
 /*
 Calculates the average from a series of values.
@@ -40,40 +10,39 @@ function getAverage(numberArray) {
     return "After dropping the two lowest values, there is nothing left to calculate. Please submit more than 2 scores."
   }
 
-  for (let x = 0; x < DROPS; x++) {
-    let min = Math.min(numberArray)
-    // Doesn't affect original array numberArray.filter(e => e != min)
-  }
-  // numberArray.sort(function(a, b) {
-  //   if (isNaN(a) || isNaN(b)) {
-  //     return "Error: Values must only contain numbers."
-  //   }
+  numberArray.sort(function(a, b) {
+    if (isNaN(a) || isNaN(b)) {
+      return "Error: Values must only contain numbers."
+    }
 
-  //   return a - b
-  // }).numberArray.splice(0, 2) // Sort the array and remove the first two items.
+    return a - b
+  }).splice(0, 2) // Sort the array and remove the first two items.
 
   // Use reduce to total numbers instead of a foreach loop. This will concat the string values, so they need to be converted to numbers.
   let total = numberArray.reduce((runningTotal, currentVal) => (+runningTotal) + (+currentVal)) // Iterate through each element and add them together.
   let average = total / numberArray.length // Basic arithmetic.
-  return average.toFixed(1) // One decimal place.
+  return average.toFixed(2) // Two decimal places.
 }
 
 // Test data for getAverage function:
-// console.log("Test 1: " + getAverage(['4', '1', '6', '10', '2', '3']))  //5.75
-// console.log("Test 2: " + getAverage([4, 1, 6, 10, 2, 3])) //5.75
-// console.log("Test 3: " + getAverage(["4", 1, "6", 10, '2', 3])) //5.75
-// console.log("Test 4: " + getAverage([4.6, 1.3, 0.6, 10.34, 2.00001, 0.0000003])) //4.56
-// console.log("Test 5: " + getAverage([-4, 1, 6, -10, 2, -3])) //1.5
-// console.log("Test 6: " + getAverage(['a', 'b', 'c', 'd', 'e', 'f'])) // Validation error message. **Fail - is returning 'NaN'.
-// console.log("Test 7: " + getAverage([])) // Validation error message.
-// console.log("Test 8: " + getAverage([1])) // Validation error message.
-// console.log("Test 9: " + getAverage([1,2])) // Validation error message.
-// console.log("Test 10: " + getAverage([1,2,3])) // 3.0
-// console.log("Test 11: " + getAverage([NaN])) // Validation error message.
-console.log("Test 12: " + getAverage([0, 0, 0, 0, 0, 0, 0])) // 
-console.log("Test 12: " + getAverage([1, 2, 1, 2, 1, 2, 1, 2])) // 
-console.log("Test 12: " + getAverage([1, 1, 2, 2, 3])) // 
-console.log("Test 12: " + getAverage([1, 1, 2, 3, 3])) // 
+console.log("Test 1: " + getAverage(['4', '1', '6', '10', '2', '3']))  //5.75
+console.log("Test 2: " + getAverage([4, 1, 6, 10, 2, 3])) //5.75
+console.log("Test 3: " + getAverage(["4", 1, "6", 10, '2', 3])) //5.75
+console.log("Test 4: " + getAverage([4.6, 1.3, 0.6, 10.34, 2.00001, 0.0000003])) //4.56
+console.log("Test 5: " + getAverage([-4, 1, 6, -10, 2, -3])) //1.5
+
+//** Fail - is returning 'NaN' **
+console.log("Test 6: " + getAverage(['a', 'b', 'c', 'd', 'e', 'f'])) // Validation error message.
+
+console.log("Test 7: " + getAverage([])) // Validation error message.
+console.log("Test 8: " + getAverage([1])) // Validation error message.
+console.log("Test 9: " + getAverage([1,2])) // Validation error message.
+console.log("Test 10: " + getAverage([1,2,3])) // 3.0
+console.log("Test 11: " + getAverage([NaN])) // Validation error message.
+console.log("Test 12: " + getAverage([0, 0, 0, 0, 0, 0, 0])) // 0.00
+console.log("Test 13: " + getAverage([1, 2, 1, 2, 1, 2, 1, 2])) // 1.67
+console.log("Test 14: " + getAverage([1, 1, 2, 2, 3])) // 2.33
+console.log("Test 15: " + getAverage([1, 1, 2, 3, 3])) // 2.67
 
 /*
 Generates an array of random numbers.
